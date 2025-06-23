23bc5f63d5297bbc0a6ac40affd9a889c3077eaf
refnandos <fernandorevollo10@gmail.com>
2025-06-23 20:05:01 +0200

    ROSAS

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,21 +1,58 @@
-import React from 'react'
-
+import { useState } from 'react';
+import './Proposal.css'; 
 export const Formulario = () => {
+
+    const [showBouquet, setShowBouquet] = useState(false);
+
+  const handleOptionChange = (e) => {
+    setShowBouquet(e.target.value === 'si');
+  };
+
+  const closeBouquet = () => {
+    setShowBouquet(false);
+    // Resetear el radio button
+    document.querySelector('input[type="radio"][value="si"]').checked = false;
+  };
 return (
-    <div className='contenedorFormulario'>
-        <form>
-            <label htmlFor="pregunta">¿quieres ser mi niña de los mechones rojos?</label>
-            <div className='contenedorRadio'>
-                <div>
-                    <input type="radio" id="opcionSi" name="opcion" value="si" />
-                    <label htmlFor="opcionSi">Sí</label>
-                </div>
-                <div>
-                    <input type="radio" id="opcionNo" name="opcion" value="no" />
-                    <label htmlFor="opcionNo">No</label>
-                </div>
-            </div>
-        </form>
+    <div className="proposal-container">
+      <form>
+        <label htmlFor="pregunta">¿Quieres ser mi niña de los mechones rojos?</label>
+        <div className='contenedorRadio'>
+          <div>
+            <input 
+              type="radio" 
+              id="opcionSi" 
+              name="opcion" 
+              value="si" 
+              onChange={handleOptionChange} 
+            />
+            <label htmlFor="opcionSi">Sí</label>
+          </div>
+          <div>
+            <input 
+              type="radio" 
+              id="opcionNo" 
+              name="opcion" 
+              value="no" 
+              onChange={handleOptionChange} 
+            />
+            <label htmlFor="opcionNo">No</label>
+          </div>
+        </div>
+      </form>
+
+      {showBouquet && (
+        <div className="bouquet">
+          <button className="close-btn" onClick={closeBouquet}>×</button>
+          <div className="roses-container">
+            {[...Array(6)].map((_, i) => (
+              <div key={i} className="rose"></div>
+            ))}
+            <div className="ribbon"></div>
+          </div>
+          <div className="message">UNAS ROSAS QUE SE HARAN REALES PARA TI </div>
+        </div>
+      )}
     </div>
-)
-}
+  );
+};
